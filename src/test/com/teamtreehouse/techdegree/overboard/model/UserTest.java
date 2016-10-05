fcbb83f3580034f6ea488df7f74d8f4ad665492b
mcanas <mcanas@cargurus.com>
2016-10-05 16:25:06 -0400

    Using a test, ensure that voting either up or down is not allowed on questions or answers by the original author, you know to avoid gaming the system. - Ensure the proper exceptions are being thrown. - Update user downVote method to meet test case. - Refactored test fixture by adding/using more stubs for test cases.

--- src/test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,25 +1,35 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
+import com.teamtreehouse.techdegree.overboard.exc.VotingException;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class UserTest {
 
     private Board board;
+    private User alice;
+    private User bob;
+    private Question question;
+    private Answer answer;
 
     @Before
     public void setUp() throws Exception {
         board = new Board("Java");
+        alice = board.createUser("alice");
+        bob = board.createUser("bob");
+        question = alice.askQuestion("What is a String?");
+        answer = bob.answerQuestion(question, "It is a series of characters, strung together...");
     }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void userReputationIncreasesWhenTheirQuestionIsUpvoted() throws Exception {
-        User alice = board.createUser("alice");
-        User bob = board.createUser("bob");
-        Question question = alice.askQuestion("What is a String?");
-
         bob.upVote(question);
 
         assertEquals("User reputation increases by 5 when their question is upvoted", 5, alice.getReputation());
@@ -27,11 +37,6 @@ public class UserTest {
 
     @Test
     public void userReputationIncreasesWhenTheirAnswerIsUpvoted() throws Exception {
-        User alice = board.createUser("alice");
-        User bob = board.createUser("bob");
-        Question question = alice.askQuestion("What is a String?");
-        Answer answer = bob.answerQuestion(question, "It is a series of characters, strung together...");
-
         alice.upVote(answer);
 
         assertEquals("User reputation increases by 10 when their answer is upvoted", 10, bob.getReputation());
@@ -39,13 +44,40 @@ public class UserTest {
 
     @Test
     public void userReputationIncreasesWhenTheirAnswerIsAccepted() throws Exception {
-        User alice = board.createUser("alice");
-        User bob = board.createUser("bob");
-        Question question = alice.askQuestion("What is a String?");
-        Answer answer = bob.answerQuestion(question, "It is a series of characters, strung together...");
-
         alice.acceptAnswer(answer);
 
         assertEquals("User reputation increases by 15 when their answer is accepted", 15, bob.getReputation());
+    }
+
+    @Test
+    public void userCannotUpVoteTheirOwnQuestion() throws Exception {
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        alice.upVote(question);
+    }
+
+    @Test
+    public void userCannotDownVoteTheirOwnQuestion() throws Exception {
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        alice.downVote(question);
+    }
+
+    @Test
+    public void userCannotUpVoteTheirOwnAnswer() throws Exception {
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        bob.upVote(answer);
+    }
+
+    @Test
+    public void userCannotDownVoteTheirOwnAnswer() throws Exception {
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        bob.downVote(answer);
     }
 }
