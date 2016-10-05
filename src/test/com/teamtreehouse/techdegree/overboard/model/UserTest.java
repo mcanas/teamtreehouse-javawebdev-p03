32cdaf4a121d5b9e6908466b95c4fbda84822aaf
mcanas <mcanas@cargurus.com>
2016-10-05 15:52:33 -0400

    Write a test to ensure that the questioner’s reputation goes up by 5 points if their question is upvoted. - Also edited .gitignore

--- src/test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,10 +1,27 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by mcanas on 10/5/16.
- */
 public class UserTest {
 
+    private Board board;
+
+    @Before
+    public void setUp() throws Exception {
+        board = new Board("Java");
+    }
+
+    @Test
+    public void userReputationIncreasesWhenTheirQuestionIsUpvoted() throws Exception {
+        User alice = board.createUser("alice");
+        User bob = board.createUser("bob");
+        Question question = alice.askQuestion("What is a String?");
+
+        bob.upVote(question);
+
+        assertEquals("User reputation increases by 5 when their question is upvoted", 5, alice.getReputation());
+    }
 }
