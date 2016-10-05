bde24938f70e4e5c273ed1579cefadf857637a3b
mcanas <mcanas@cargurus.com>
2016-10-05 15:56:17 -0400

    Write a test to assert that the answerer’s reputation goes up by 10 points if their answer is upvoted.

--- src/test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -24,4 +24,16 @@ public class UserTest {
 
         assertEquals("User reputation increases by 5 when their question is upvoted", 5, alice.getReputation());
     }
+
+    @Test
+    public void userReputationIncreasesWhenTheirAnswerIsUpvoted() throws Exception {
+        User alice = board.createUser("alice");
+        User bob = board.createUser("bob");
+        Question question = alice.askQuestion("What is a String?");
+        Answer answer = bob.answerQuestion(question, "It is a series of characters, strung together...");
+
+        alice.upVote(answer);
+
+        assertEquals("User reputation increases by 10 when their answer is upvoted", 10, bob.getReputation());
+    }
 }
